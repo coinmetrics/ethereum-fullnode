619b8cf58c117cb44f4a3fb905edc5a55ce931a7
Quyse Lert <quyse0@gmail.com>
2018-11-11 14:25:21 +0300

    build config

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "Building image..."
+docker build -t "${DOCKER_REGISTRY_REPO}" .
+echo "Image ready."
+
+if [ -n "${DOCKER_REGISTRY}" ] && [ -n "${DOCKER_REGISTRY_USER}" ] && [ -n "${DOCKER_REGISTRY_PASSWORD}" ]
+then
+	echo "Logging into ${DOCKER_REGISTRY}..."
+	exit 0
+	docker login -u="${DOCKER_REGISTRY_USER}" -p="${DOCKER_REGISTRY_PASSWORD}" "${DOCKER_REGISTRY}"
+	echo "Pushing image to ${DOCKER_REGISTRY_REPO}..."
+	docker push ${DOCKER_REGISTRY_REPO}:latest
+fi
